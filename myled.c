a03c538b50f101338382f4dbaea679bd25fe3735
kobayashishu <42289418+shu-kobayashi@users.noreply.github.com>
2018-12-30 18:43:15 +0900

    コメントアウトとauthorの訂正

--- myled.c
+++ myled.c
@@ -1,4 +1,5 @@
-/*Copyright (c) 2018 Shu Kobayashi
+/*
+Copyright (c) 2018 Shu Kobayashi
 
 This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
